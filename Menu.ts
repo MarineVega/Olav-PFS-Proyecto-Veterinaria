77170ecd47aa4e8652445266b53df440fcb29710
AntonelaDiPinto <dipintoantonela@gmail.com>
2024-11-22 02:48:55 -0300

    Se crea el archivo Menu donde se encuentra el código del menú de la consola a modo de prueba.

---
+++ Menu.ts
@@ -0,0 +1,230 @@
+import * as readline from 'readline';
+import { Persona } from "./Persona";
+import { Cliente } from "./Cliente";
+import { Paciente } from "./Paciente";
+import { Proveedor } from "./Proveedor";
+import { Sucursal } from "./Sucursal";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  // Datos globales
+let sucursales: Sucursal[] = [];
+let listaProvedores: Proveedor[] = [];
+let listaClientes: Cliente[] = [];
+let listaPacientes: Paciente[] = [];
+
+function mostrarMenu() {
+  console.log(`
+  1. Crear Sucursal
+  2. Crear Proveedor
+  3. Crear Cliente
+  4. Crear Paciente
+  5. Mostrar Sucursales
+  6. Mostrar Provedores
+  7. Mostrar Clientes
+  8. Mostrar Pacientes
+  9. Salir
+  `);
+}
+
+function leerOpcion() {
+    console.log('Bienvenido a 🐾 🐈 Veterinaria Pocas Pulgas 🐩 🐾');
+    rl.question('Selecciona una opción: ', (opcion) => {
+        switch (opcion) {
+            case '1':
+                crearSucursal();
+                break;
+            case '2':
+                crearProveedor();
+                break;
+            case '3':
+                crearCliente();
+                break;
+            case '4':
+                crearPaciente();
+                break;
+            case '5':
+                mostrarSucursales();
+                break;
+            case '6':
+                mostrarListaProvedores();
+                break;
+            case '7':
+                mostrarListaClientes();
+                break;
+            case '8':
+                mostrarListaPacientes();
+                break;
+            case '9':
+                rl.close();
+                console.log("Salió correctamente del Sistema.\nGracias por ser parte de 🐾 🐈 Veterinaria Pocas Pulgas 🐩 🐾.")
+                break;
+            default:
+                console.log('Opción no válida');
+                leerOpcion();
+        }
+    });
+}
+
+let id = 0; // Inicializa el ID global para todas las clases?
+
+//CREAMOS LA SUCURSAL
+function crearSucursal() {
+  console.log(`ID Sucursal: ${id + 1}`); 
+  id += 1; // Incrementar el ID global para el siguiente elemento que se crea
+  rl.question('Nombre del Responsable de la Sucursal: ', (responsable) => {
+    rl.question('Dirección de la sucursal: ', (direccion: string) =>  {
+        rl.question('Localidad en la que se encuentra la sucursal: ', (localidad) =>  {   
+      const sucursal = new Sucursal(id, responsable, direccion, localidad);
+      sucursales.push(sucursal);
+      console.log('Sucursal creada exitosamente.');
+      //console.log('-------------------------------------------------------------------------');
+      console.log("🐾 🐾 🐾 🐾 🐾 🐾 🐾 🐾 🐾 🐾");
+      leerOpcion();
+        });
+    });
+  });
+}
+
+//CREAMOS PROVEEDORES
+function crearProveedor() {
+    console.log(`ID Proveedor: ${id + 1}`); 
+    id += 1; 
+    rl.question('Nombre del proveedor: ', (nombre) => {
+      rl.question('Dirección del proveedor: ', (direccion) => {
+        rl.question('Número de teléfono (sin guiones ni espacios): ', (telefonoStr) => {
+          const telefono = parseInt(telefonoStr); 
+          if (isNaN(telefono) || telefonoStr.length < 10) {
+            console.log('Por favor, ingresa un número de teléfono válido (al menos 10 dígitos).');
+            return crearProveedor(); 
+          }
+          rl.question('Documento del proveedor: ', (documentoStr) => {
+              const documento = parseInt(documentoStr);
+              if (isNaN(documento) ||documentoStr.length < 5) {  
+                console.log('Por favor, ingresa un número de documento válido (al menos 5 dígitos).');
+                return crearProveedor(); 
+              }
+              rl.question('Rubro del proveedor: ', (rubro) => {
+                rl.question('CUIT del proveedor: ', (CUITStr) => {
+                    const CUIT = parseInt(CUITStr);
+                    if (isNaN(CUIT) ||CUITStr.length < 11) {  
+                        console.log('Por favor, ingresa un número de CUIT válido (11 dígitos).');
+                        return crearProveedor(); 
+                      }
+            const proveedor = new Proveedor(id, nombre, direccion, telefono, documento, rubro, CUIT);
+            listaProvedores.push(proveedor);  // Suponiendo que tienes un array 'listaClientes'
+            console.log('Proveedor creado exitosamente.');
+           //console.log('------------------------------------------------------------------');
+           console.log("🐾 🐾 🐾 🐾 🐾 🐾 🐾 🐾 🐾 🐾");
+            leerOpcion();  
+                    });
+              });
+          });
+        });
+      });
+    });
+  }
+
+//CREAMOS CLIENTES
+function crearCliente() {
+  console.log(`ID Cliente: ${id + 1}`); // Mostrar el próximo ID para el cliente
+  id += 1; // Incrementar el ID global para el siguiente cliente
+  rl.question('Nombre del cliente: ', (nombre) => {
+    rl.question('Dirección del cliente: ', (direccion) => {
+      rl.question('Número de teléfono (sin guiones ni espacios): ', (telefonoStr) => {
+        const telefono = parseInt(telefonoStr);  // Conversión de string a number
+        if (isNaN(telefono) || telefonoStr.length < 10) {
+          console.log('Por favor, ingresa un número de teléfono válido (al menos 10 dígitos).');
+          return crearCliente();  // Volver a preguntar si el teléfono no es válido
+        }
+        rl.question('Documento del cliente: ', (documentoStr) => {
+            const documento = parseInt(documentoStr);
+            // Aquí puedes hacer más validaciones dependiendo de la estructura de tu documento
+            if (isNaN(documento) ||documentoStr.length < 5) {  // Suponiendo que el documento debe tener al menos 5 dígitos
+              console.log('Por favor, ingresa un número de documento válido (al menos 5 dígitos).');
+              return crearCliente();  // Volver a preguntar si el documento no es válido
+            }
+          const cliente = new Cliente(id, nombre, direccion, telefono, documento);
+          listaClientes.push(cliente);  // Suponiendo que tienes un array 'listaClientes'
+          console.log('Cliente creado exitosamente.');
+          //console.log('------------------------------------------------------------------');
+          console.log("🐾 🐾 🐾 🐾 🐾 🐾 🐾 🐾 🐾 🐾");
+          leerOpcion();  // Volver a mostrar el menú de opciones
+        });
+      });
+    });
+  });
+}
+
+function crearPaciente() {
+    console.log(`ID Paciente: ${id + 1}`); 
+    id += 1; 
+    rl.question('Nombre del paciente: ', (nombre) => {
+      rl.question('Especie del paciente: ', (especie) => {
+        rl.question('Sexo del paciente: ', (sexo) => {
+          if (sexo !== 'Macho' && sexo !== 'Hembra') {
+            console.log('Por favor, ingresa un sexo válido: Macho o Hembra.');
+            return crearPaciente(); 
+          }
+          rl.question('Fecha de nacimiento del paciente (DD-MM-YYYY): ', (fechaStr) => {
+            const fechaNacimiento = new Date(fechaStr);
+            if (isNaN(fechaNacimiento.getTime())) {
+              console.log('Por favor, ingresa una fecha válida en el formato DD-MM-YYYY.');
+              return crearPaciente();
+            }
+            rl.question('Observación del paciente: ', (observacion) => {
+              const paciente = new Paciente(id, nombre, especie, sexo, fechaNacimiento, observacion);
+              listaPacientes.push(paciente);  
+              console.log('Paciente creado exitosamente.');
+              //console.log('------------------------------------------------------------------');
+              console.log("🐾 🐾 🐾 🐾 🐾 🐾 🐾 🐾 🐾 🐾");
+              leerOpcion();  
+            });
+          });
+        });
+      });
+    });
+  }
+
+//MOSTRAMOS LAS SUCURSALES CREADAS (LISTA DE SUCURSALES)
+function mostrarSucursales() {
+    console.log(sucursales);
+    console.log('-------------------------------------------------------------------------');
+leerOpcion();
+}
+
+//MOSTRAMOS LOS PROVEEDORES CREADOS (LISTA DE PROVEEDORES)
+function mostrarListaProvedores() {
+    console.log(listaProvedores);
+    console.log('-------------------------------------------------------------------------');
+    leerOpcion();
+}
+
+//MOSTRAMOS LOS CLIENTES CREADOS (LISTA DE CLIENTES)
+function mostrarListaClientes() {
+    console.log(listaClientes);
+    console.log('-------------------------------------------------------------------------');
+    leerOpcion();
+}
+
+//MOSTRAMOS LOS PACIENTES CREADOS (LISTA DE PACIENTES)
+function mostrarListaPacientes() {
+    console.log(listaPacientes);
+    console.log('-------------------------------------------------------------------------');
+    leerOpcion();
+}
+
+/*function mostrarSucursales() {
+  console.log('Lista de sucursales:');
+  sucursales.forEach((sucursal) => {
+    sucursal.mostrarDatosSucursal();
+    console.log('---');
+  });
+  leerOpcion();
+}*/
+
+mostrarMenu();
+leerOpcion();
